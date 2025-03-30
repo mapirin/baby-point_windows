0aded7ea95594ded0d87d5a541782bce13b95d74
mapirin <812matahiro29@gmail.com>
2025-03-30 12:31:55 +0900

    point add commit

--- stockOperate.js
+++ stockOperate.js
@@ -3,9 +3,9 @@ const urlParams = new URLSearchParams(window.location.search);
 let point = Number(urlParams.get("point"))
 
 returnButton.addEventListener('click', ()=>{
-    const selectedRadio = document.querySelectorAll('input[name="stockOptions"]:checked');
+    const selectedRadio = document.querySelector('input[name="stockOptions"]:checked');
     if(selectedRadio){
-        point = point + selectedRadio.value;
+        point = point + Number(selectedRadio.value);
         window.opener.postMessage({updatePoint:point}, window.location.origin);
         window.close();
     }else{
